02e4ec0c210249abe2495d73cfa84ab468ed7cdd
lizhaoyang125 <banbingshu@163.com>
2025-03-18 21:38:28 +0800

    remove useless 文件

--- Ambitious2d/assets/Pictures/Maps/Modern_Office_MV_2_TILESETS_B-C-D-E.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Modern_Office_MV_2_TILESETS_B-C-D-E" tilewidth="48" tileheight="48" tilecount="256" columns="16">
- <image source="../../../../../素材/ITHO像素游戏素材/ModernOffice/5_Modern_Office_RPG_MAKER_MV/Modern_Office_MV_2_TILESETS_B-C-D-E.png" width="768" height="768"/>
-</tileset>
